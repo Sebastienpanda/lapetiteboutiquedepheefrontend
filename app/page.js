aa0be8f613bbae3a4b8f181333e171155e8465e2
SebastienWeb <sebastienweb27@gmail.com>
2024-01-17 00:29:21 +0100

    Update file page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import { Category } from "../components/layout/category.jsx";
 import { HeroBanner } from "../components/layout/heroBanner.jsx";
+import { Reseaux } from "../components/layout/reseaux.jsx";
 import { SelectionOfMoment } from "../components/layout/selectionOfMoment.jsx";
+import { Articles } from "../components/layout/slider/articles.jsx";
+import { SommeNous } from "../components/layout/sommesNous.jsx";
 import { Trust } from "../components/layout/trust.jsx";
 
 export default function Home() {
@@ -11,6 +14,9 @@ export default function Home() {
         <Category />
         <SelectionOfMoment />
         <Trust />
+        <SommeNous />
+        <Articles />
+        <Reseaux />
       </main>
     </>
   );
