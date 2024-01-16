00280e062cb3b2577f46b1552bcc1322d8e0e848
SebastienWeb <sebastienweb27@gmail.com>
2024-01-16 19:38:19 +0100

    Add multiple component to final result

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
+import { Category } from "../components/layout/category.jsx";
 import { HeroBanner } from "../components/layout/heroBanner.jsx";
+import { SelectionOfMoment } from "../components/layout/selectionOfMoment.jsx";
+import { Trust } from "../components/layout/trust.jsx";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <main className="container">
+        <Category />
+        <SelectionOfMoment />
+        <Trust />
+      </main>
     </>
   );
 }
